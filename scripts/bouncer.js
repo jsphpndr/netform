53c1142a09af5950f5efc900e6a90c2d796af5f6
Joseph Pinder <hello@josephpinder.com>
2021-05-26 21:19:46 -0400

    trying something

--- scripts/bouncer.js
+++ scripts/bouncer.js
@@ -861,13 +861,20 @@ var bouncer = new Bouncer('[data-validate]', {
 });
 
 document.addEventListener('bouncerFormValid', function () {
-	let form = document.querySelector('[data-validate]');
-	// HTMLFormElement.prototype.submit.call(form);
-  let formData = new FormData(form)
-  fetch('/', {
-    method: 'POST',
-    headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    body: new URLSearchParams(formData).toString()
+	
+  let form = document.querySelector('[data-validate]');
+	
+  HTMLFormElement.prototype.submit.call(form, function () {
+
+    let formData = new FormData(form);
+
+    fetch('/', {
+      method: 'POST',
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams(formData).toString()
+    });
+
   });
+  
 	// window.location = "/thank-you";
 }, false);
